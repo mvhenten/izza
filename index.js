153294258116dad7081e6efe75cac0628303d047
Matthijs van Henten br/ <matthijs@technischepartij.nl>
2015-09-04 22:10:28 +0000

    rewrite: ext deps; better errors

--- index.js
+++ index.js
@@ -1,69 +1,78 @@
 'use strict';
 
 var isPlainFunction = require('isa-plain-function');
-var format = require('./lib/util').format;
-var typeName = require('./lib/util').typeName;
-var valueName = require('./lib/util').valueName;
+var format = require('util').format;
+var partal = require('partal');
+var nameType = require("type-name");
 
-function formatError(name) {
-    return format('%s "%s"', /^[aoeu]/.test(name.toLowerCase()) ? 'an' : 'a', name);
+function name(val) {
+    if (nameType(val) === "function" && val.name)
+        return val.name;
+
+    if (typeof val === 'object' && val) {
+        // its an instance of something
+        var ctr = val.constructor;
+
+        while (ctr) {
+            if (typeof ctr === "function" && ctr.name)
+                return "[instanceof " + name(ctr) + "]";
+            ctr = ctr.constructor;
+        }
+    }
+
+    return nameType(val);
 }
 
+
 function check(value, type) {
-    if (type instanceof RegExp) {
-        if (!type.test(value)) return format('a value matching "%s"', type);
-        return;
-    }
+    if (type instanceof RegExp)
+        return type.test(value) ? format('a value matching "%s"', type) : undefined;
 
     if (/^(Boolean|Number|String|RegExp|Array|Object|Date|Function)$/.test(type.name)) {
         if (typeof value === type.name.toLowerCase()) return;
         if ((value instanceof type)) return;
 
-        return formatError(typeName(type));
+        return name(type);
     }
 
-    if (isPlainFunction(type) && type.name) {
-        var err = type(value);
+    if (isPlainFunction(type))
+        return type(value);
 
-        if (err === true) return;
-
-        if (err instanceof TypeError)
-            return err;
-
-        return formatError(typeName(type));
-    }
-
-    if (value instanceof type) return;
-
-    return format('an instanceof "%s"', typeName(type));
+    if (!(value instanceof type))
+        return name(type);
 }
 
+
 var Izza = {
-    check: function(value, type) {
+    check: function(type, value) {
         if (!((type instanceof Function) || (type instanceof RegExp)))
-            return format('Input Error: type is not an instanceof Function');
+            return new TypeError('First argument "type" is not an instanceof Function');
 
         var err = check(value, type);
 
-        if (!err) return true;
-
-        if (err instanceof TypeError)
+        if (err instanceof Error)
             return err;
 
-        return format('"%s" is not %s, it is a "%s"', value, err, valueName(value));
+        if (err === false)
+            err = name(type);
+
+        if (typeof err === "string")
+            return new TypeError(format('"%s" is not a "%s", it is a "%s"', value, err, name(value)));
     },
 
     isa: function(type, value, done) {
-        var err = Izza.check(value, type);
+        if (arguments.length == 1)
+            return partal(Izza.isa, type);
 
-        if (done instanceof Function)
-            return done(err === true ? null : err, value, type);
+        if (arguments.length == 2)
+            return !(Izza.check(type, value) instanceof Error);
 
-        if (err instanceof TypeError)
-            throw err;
+        return process.nextTick(partal(done, Izza.check(type, value)));
+    },
 
-        if (err !== true) throw new TypeError(err);
-        return true;
+    assert: function(type, value) {
+        var err = Izza.check(type, value);
+        if (err) throw err;
     }
 };
 
