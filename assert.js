3ec9f7d572f404c8cd5ab97ae4d9541525f1ab84
Matthijs van Henten <matthijs+foo@technischepartij.nl>
2017-05-08 09:52:14 +0000

    allow custom assert messages

--- assert.js
+++ assert.js
@@ -2,9 +2,16 @@
 
 const check = require("./check");
 
-module.exports = function assertType(type, value) {
+module.exports = function assertType(type, value, message) {
     const err = check(type, value);
     
-    if (err) throw err;
+    if (!err) return;
+    
+    if (message)
+        err.message = `${message}: ${err.message}`;
+        
+    err.name = "AssertionError";
+    
+    throw err;
 };
 
--- assert_test.js
+++ assert_test.js
@@ -1,10 +1,21 @@
-  'use strict';
+'use strict';
 
- const test = require('tape'),
-     assertType = require('./assert');
+const test = require('tape');
+const assertType = require('./assert');
 
 test("assertType throws", (assert) => {
-    assert.throws(() => assertType(Number, "1"), 'TypeError: "1" is not a "Number", it is a "string"');
+    assert.throws(() => assertType(Number, "1"),
+        /AssertionError: "1" is not a "Number", it is a "string"/
+    );
     assert.end();
 });
-     
+
+
+
+
+test("assertType throws custom message", (assert) => {
+    assert.throws(() => assertType(Number, "1", '"foo" is a number'),
+        /AssertionError: "foo" is a number: "1" is not a "Number", it is a "string"/
+    );
+    assert.end();
+});
