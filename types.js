d6fa37dc5265fc0a1ca633172b0ee1e36feb45a3
Matthijs van Henten <mvhenten@users.noreply.github.com>
2017-09-03 00:42:15 +0200

    Fix order for the custom types
    changed api for check to do type first

--- types.js
+++ types.js
@@ -5,7 +5,7 @@ const format = require('util').format;
 
 function Tuple(...types) {
     return function Tuple(value) {
-        const err = check(value, Array);
+        const err = check(Array, value);
 
         if (err !== true)
             return new TypeError("Invalid Tuple: value should be an array");
@@ -14,7 +14,7 @@ function Tuple(...types) {
             return new TypeError(format('Invalid Tuple: expected %s items, got %s', types.length, value.length));
 
         for (const i = 0; i < types.length; i++) {
-            err = check(value[i], types[i]);
+            err = check(types[i], value[i]);
             if (err !== true)
                 return new TypeError(format('Invalid Tuple: item %s: %s', i, err));
         }
@@ -29,7 +29,7 @@ function Maybe(type) {
         if (value === null || value === undefined)
             return true;
 
-        const err = check(value, type);
+        const err = check(type, value);
 
         if (err === true)
             return err;
